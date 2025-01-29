5b6cedc25c0ea6da1e14f2ad2c20924c7d1ab020
shiyinxi <shiyinxi@gmail.com>
2025-01-28 22:55:59 -0800

    complete shoppinglist app

--- ShoppingList/urls.py
+++ ShoppingList/urls.py
@@ -5,6 +5,5 @@ urlpatterns=[
     path('', views.get_shopping_list, name='shopping_list'),
     path('inventory/<int:item_id>/', views.add_item_to_inventory, name='add_item_to_inventory'),
     path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
-    path('add/', views.add_item, name='add_item'),
 
 ]
--- ShoppingList/views.py
+++ ShoppingList/views.py
@@ -7,6 +7,7 @@ from Inventory.models import Inventory
 from Inventory.serializers import InventorySerializer
 from rest_framework.parsers import JSONParser
 from Recipes.models import Ingredient
+from django.shortcuts import get_object_or_404
 
 @csrf_exempt
 def get_shopping_list(request):
@@ -21,49 +22,23 @@ def get_shopping_list(request):
         return json_response({"error": "Invalid HTTP method"}, status=405)
     
 @csrf_exempt
-def add_item_to_inventory(request, ingredient_id):
+def add_item_to_inventory(request, item_id):
     if request.method == "POST":
         try:
-            shopping_list_item = ShoppingList.objects.get(ingredient_id=ingredient_id)
+            shopping_list_item = ShoppingList.objects.get(id=item_id)
             ingredient = shopping_list_item.ingredient
             quantity = shopping_list_item.quantity
 
-            # Check if the ingredient already exists in the inventory
             inventory_item, created = Inventory.objects.get_or_create(ingredient=ingredient, defaults={'quantity': quantity})
             if not created:
-                # If the ingredient already exists, update the quantity
-                inventory_item.quantity = quantity
+                inventory_item.quantity += quantity
                 inventory_item.save()
-
-            # Remove the ingredient from the shopping list
             shopping_list_item.delete()
 
             serializer = InventorySerializer(inventory_item)
             return json_response(serializer.data, status=201)
         except ShoppingList.DoesNotExist:
             return json_response({"error": "Ingredient not found in shopping list"}, status=404)
-        except Exception as e:
-            return handle_exception(e)
-    else:
-        return json_response({"error": "Invalid HTTP method"}, status=405)
-    
-@csrf_exempt
-def add_item(request):
-    if request.method == "POST":
-        try:
-            data = JSONParser().parse(request)
-            ingredient_data = data.get('ingredient')
-            quantity = data.get('quantity')
-
-            ingredient, created = Ingredient.objects.get_or_create(name=ingredient_data['name'], defaults={'quantity': ingredient_data['quantity']})
-            shopping_list_item, created = ShoppingList.objects.get_or_create(ingredient=ingredient, defaults={'quantity': quantity})
-
-            if not created:
-                shopping_list_item.quantity = quantity
-                shopping_list_item.save()
-
-            serializer = ShoppingListSerializer(shopping_list_item)
-            return json_response(serializer.data, status=201)
         except Exception as e:
             return handle_exception(e)
     else:
